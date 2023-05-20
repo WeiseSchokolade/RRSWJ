8386cc92254f92ea2d3c5e169f601bf66f40d425
WeiseSchokolade <99491985+WeiseSchokolade@users.noreply.github.com>
2023-05-20 19:28:20 +0200

    Removed StrokeWidth Caching

--- src/main/java/de/schoko/rendering/hud/LineDrawCall.java
+++ src/main/java/de/schoko/rendering/hud/LineDrawCall.java
@@ -5,7 +5,6 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 
 public class LineDrawCall extends DrawCall {
-	protected static float lastRecordedStrokeWidth;
 	private int x0, y0;
 	private int x1, y1;
 	private Color color;
@@ -31,12 +30,7 @@ public class LineDrawCall extends DrawCall {
 	@Override
 	public void call(Graphics2D g2D) {
 		g2D.setColor(color);
-		if (width != -1) {
-			if (width != lastRecordedStrokeWidth) {
-				g2D.setStroke(new BasicStroke(width));
-				lastRecordedStrokeWidth = width;
-			}
-		}
+		g2D.setStroke(new BasicStroke(width));
 		g2D.drawLine(x0, y0, x1, y1);
 	}
 
