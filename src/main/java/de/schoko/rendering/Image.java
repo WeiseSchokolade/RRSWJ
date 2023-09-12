010aef606fa86e3ef529b09075ad442cde88ec4d
WeiseSchokolade <99491985+WeiseSchokolade@users.noreply.github.com>
2023-09-12 15:49:38 +0200

    Added image turning methods
    Signed-off-by: WeiseSchokolade <99491985+WeiseSchokolade@users.noreply.github.com>

--- src/main/java/de/schoko/rendering/Image.java
+++ src/main/java/de/schoko/rendering/Image.java
@@ -3,6 +3,8 @@ package de.schoko.rendering;
 import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.WritableRaster;
 
 public class Image {
 	private String name;
@@ -34,12 +36,23 @@ public class Image {
 		return name;
 	}
 	
+	private Image copy(boolean copyBufferedImage) {
+		Image copy = new Image(this.name, bufferedImage, width, height);
+		if (!copyBufferedImage) {
+			ColorModel model = copy.bufferedImage.getColorModel();
+			WritableRaster raster = copy.bufferedImage.copyData(null);
+			boolean alphaPremultiplied = model.isAlphaPremultiplied();
+			copy.bufferedImage = new BufferedImage(model, raster, alphaPremultiplied, null);
+		}
+		return copy;
+	}
+	
 	/**
 	 * Flips the current image horizontally.
 	 * @return The flipped image
 	 */
 	public Image flipHorizontally() {
-		Image copy = new Image(name, this.bufferedImage, width, height);
+		Image copy = copy(true);
 		AffineTransform txTransform = AffineTransform.getScaleInstance(-1, 1);
 		txTransform.translate(-width, 0);
 		AffineTransformOp op = new AffineTransformOp(txTransform, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
@@ -52,10 +65,34 @@ public class Image {
 	 * @return The flipped image
 	 */
 	public Image flipVertically() {
-		Image copy = new Image(name, this.bufferedImage, width, height);
+		Image copy = copy(true);
 		AffineTransform txTransform = AffineTransform.getScaleInstance(1, -1);
 		txTransform.translate(0, -height);
 		AffineTransformOp op = new AffineTransformOp(txTransform, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
+		copy.bufferedImage = op.filter(copy.bufferedImage, null);
+		return copy;
+	}
+	
+	/**
+	 * @return A copy of the image rotated clockwise
+	 */
+	public Image turnClockwise() {
+		Image copy = copy(true);
+		AffineTransform transform = new AffineTransform();
+		transform.rotate(Math.PI * 0.5, copy.width * 0.5, copy.height * 0.5);
+		AffineTransformOp op = new AffineTransformOp(transform, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
+		copy.bufferedImage = op.filter(copy.bufferedImage, null);
+		return copy;
+	}
+
+	/**
+	 * @return A copy of the image rotated counter clockwise
+	 */
+	public Image turnCounterClockwise() {
+		Image copy = copy(true);
+		AffineTransform transform = new AffineTransform();
+		transform.rotate(Math.PI * -0.5, copy.width * 0.5, copy.height * 0.5);
+		AffineTransformOp op = new AffineTransformOp(transform, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
 		copy.bufferedImage = op.filter(copy.bufferedImage, null);
 		return copy;
 	}
